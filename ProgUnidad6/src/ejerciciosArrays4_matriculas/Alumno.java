0f7c0b4156adc85a2b21e6981de6eb186c4a5112
Robert Gutiérrez <jrgutinez@gmail.com>
2022-01-08 20:05:39 +0100

    var updates

---
+++ ProgUnidad6/src/ejerciciosArrays4_matriculas/Alumno.java
@@ -0,0 +1,95 @@
+package ejerciciosArrays4_matriculas;
+
+public class Alumno {
+	/**
+	 * Practicando con Arrays Matrículas
+	 * 
+	 * @author Robert G
+	 */
+	public String clave;
+	public String dni;
+	public String email;
+	public String nombre;
+
+	public Alumno(String clave, String dni, String email, String nombre) {
+		super();
+		this.clave = clave;
+		this.dni = dni;
+		this.email = email;
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [clave=" + clave + ", dni=" + dni + ", email=" + email + ", nombre=" + nombre + "]";
+	}
+
+	public Alumno() {
+		super();
+		this.clave = "";
+		this.dni = "";
+		this.email = "";
+		this.nombre = "";
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int hashCode(Alumno al) {
+		return al.getDni().hashCode();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumno other = (Alumno) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+}
---
+++ ProgUnidad6/src/ejerciciosArrays4_matriculas/MatriculaAlumnos_01.java
@@ -0,0 +1,117 @@
+package ejerciciosArrays4_matriculas;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class MatriculaAlumnos_01 {
+	/**
+	 * Practicando con Arrays Matrículas
+	 * 
+	 * @author Robert G
+	 */
+	static LinkedList<Alumno> AlumnosPreinscritos = new LinkedList<Alumno>();
+	static LinkedList<Alumno> AlumnosMatriculados = new LinkedList<Alumno>();
+
+	public static void main(String[] args) {
+
+		boolean salir = true;
+		Scanner sc = new Scanner(System.in);
+		String opcion = "";
+		generarListas(AlumnosPreinscritos);
+
+		// Muestro en consola los alumnos preinscritos
+		System.out.println("Listado de alumnos preinscritos:\n" + "********************************");
+		for (Alumno a : AlumnosPreinscritos)
+			System.out.println(a);
+		do {
+			System.out.println("\n1) Matricular alumno");
+			System.out.println("2) Borrar alumno matriculado con dni");
+			System.out.println("3) Mostrar ultimo alumno matriculado");
+			System.out.println("4) Mostrar alumnos matriculados");
+			System.out.println("5) Salir");
+			System.out.println("Elige una opción");
+			opcion = sc.next();
+
+			switch (opcion) {
+
+			case "1":
+				if (AlumnosPreinscritos.isEmpty()) {
+					System.out.println("No hay alumnos preinscritos");
+				} else {
+					AlumnosMatriculados.addLast(AlumnosPreinscritos.getFirst());
+					AlumnosPreinscritos.removeFirst();
+					System.out.println("////////////////////////////////");
+					System.out.println("Alumno matriculado correctamente");
+					System.out.println("////////////////////////////////");
+				}
+				break;
+			case "2":
+				borrarAlumno(AlumnosMatriculados, AlumnosPreinscritos);
+				System.out.println("////////////////////////////");
+				System.out.println("Alumno borrado correctamente");
+				System.out.println("////////////////////////////");
+				break;
+			case "3":
+				if (!AlumnosMatriculados.isEmpty())
+					System.out.println("////////////////////////////");
+				System.out.println("Últimos alumnos matriculados");
+				System.out.println("////////////////////////////");
+				System.out.println(AlumnosMatriculados.getLast());
+				break;
+			case "4":
+				System.out.println("/////////////////////////////");
+				System.out.println("Lista de alumnos matriculados");
+				System.out.println("/////////////////////////////");
+				if (!AlumnosMatriculados.isEmpty())
+					for (Alumno a : AlumnosMatriculados)
+						System.out.println(a);
+				break;
+			case "5":
+				salir = false;
+				System.out.println("Ha salido del programa - Hasta pronto.");
+				break;
+			default:
+				System.out.println("Opción incorrecta");
+				break;
+			}
+		} while (salir);
+		sc.close();
+	}// Fin main
+
+	// Deberes: Cargar desde fichero....
+	public static void generarListas(LinkedList<Alumno> lista) {
+
+		Alumno a1 = new Alumno("juan123", "11111111H", "juan@gmail.com", "Juan");
+		Alumno a2 = new Alumno("lucia123", "22222222G", "lucia@gmail.com", "Lucia");
+		Alumno a3 = new Alumno("sara123", "33333333Z", "sara@gmail.com", "Sara");
+		Alumno a4 = new Alumno("carlos123", "44444444W", "carlos@gmail.com", "Carlos");
+		Alumno a5 = new Alumno("carmen123", "55555555", "carmen@gmail.com", "Carmen");
+		Alumno a6 = new Alumno("antonio123", "66666666Y", "antonio@gmail.com", "Antonio");
+
+		lista.add(a1);
+		lista.add(a2);
+		lista.add(a3);
+		lista.add(a4);
+		lista.add(a5);
+		lista.add(a6);
+	}
+
+	@SuppressWarnings("resource")
+	public static void borrarAlumno(LinkedList<Alumno> lista1, LinkedList<Alumno> lista2) {
+
+		Scanner sc = new Scanner(System.in);
+		String dni = "";
+		System.out.println("Introduce dni del alumno a borrar");
+		dni = sc.next();
+
+		for (Alumno a : lista1) {// Bucle for each
+			if (a.getDni().equalsIgnoreCase(dni)) {
+				lista1.remove(a);
+				if (lista2.isEmpty()) {
+					lista1.addLast(lista2.getFirst());
+					lista2.removeFirst();
+				} // Fin segundo if
+			} // Fin primer if
+		} // Fin for each
+	}// Fin Método borrarAlumno
+}// Fin Clase
